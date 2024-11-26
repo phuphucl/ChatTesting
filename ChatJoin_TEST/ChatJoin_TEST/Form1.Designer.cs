5978e92a40e947e8e19deb28c6183af01e3f3096
phuphucl <92769960+phuphucl@users.noreply.github.com>
2024-11-25 18:54:55 -0700

    Add files via upload

---
+++ ChatJoin_TEST/ChatJoin_TEST/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace ChatJoin_TEST
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.LblName = new System.Windows.Forms.Label();
+            this.LblTxt = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(8, 8);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 31);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Show Window";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // LblName
+            // 
+            this.LblName.AutoSize = true;
+            this.LblName.Location = new System.Drawing.Point(114, 17);
+            this.LblName.Name = "LblName";
+            this.LblName.Size = new System.Drawing.Size(35, 13);
+            this.LblName.TabIndex = 1;
+            this.LblName.Text = "Name";
+            // 
+            // LblTxt
+            // 
+            this.LblTxt.AutoSize = true;
+            this.LblTxt.Location = new System.Drawing.Point(114, 42);
+            this.LblTxt.Name = "LblTxt";
+            this.LblTxt.Size = new System.Drawing.Size(35, 13);
+            this.LblTxt.TabIndex = 2;
+            this.LblTxt.Text = "label1";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.richTextBox1.Location = new System.Drawing.Point(158, 47);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(523, 68);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(158, 121);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(100, 50);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(701, 370);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.LblTxt);
+            this.Controls.Add(this.LblName);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label LblName;
+        private System.Windows.Forms.Label LblTxt;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+    }
+}
+
---
+++ ChatJoin_TEST/ChatJoin_TEST/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Windows.Forms.VisualStyles;
+using PLLibrary;
+
+namespace ChatJoin_TEST
+{
+#pragma warning disable 
+    public unsafe partial class Form1 : Form
+    {
+        List<CWindow> children;
+        CProcessWrapper process;
+        int childIdx;
+        public Form1()
+        {
+            InitializeComponent();
+            CProcessList list = PLLibrary.CProcessList.Processes;
+            list = list.RemoveZeroHandle();
+            process = list["explorer"]; 
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (children == null)
+            {
+                CWindow window = new CWindow(process);
+                children = window.GetChildWindows();
+                childIdx = 0;
+            }
+            if (childIdx >= children.Count)
+            {
+                childIdx = 0;
+            }
+            CWindow childWindow = children[childIdx];
+            childIdx++;
+            LblName.Text = childIdx + "/" + children.Count + ". " + childWindow.ClassName + ", " + childWindow.Handle + ", " + childWindow.Bounds;
+            Rectangle rc = childWindow.Bounds;
+            richTextBox1.Text = childWindow.Text;
+            pictureBox1.Image = childWindow.Image;
+
+            IntPtr parent = childWindow.Parent;
+            childWindow.Parent = richTextBox1.Handle;
+            childWindow.Parent = parent;
+
+        }
+    }
+}
